dacf1fef8f092c7299e04ba3572634149b6c4885
levichen <levichen@nine-yi.com>
2018-05-14 13:15:02 +0800

    finished 0021

--- 0021_MergeTwoSortedLists2.js
+++ 0021_MergeTwoSortedLists2.js
@@ -10,32 +10,16 @@ function ListNode (val) {
  * @return {ListNode}
  */
 var mergeTwoLists = function (l1, l2) {
-  let result = new ListNode(0)
-  let current = result
-
   if (l2 === null) return l1
   if (l1 === null) return l2
 
-  while (l1 !== null && l2 !== null) {
-    if (l1.val < l2.val) {
-      current.next = new ListNode(l1.val)
-      l1 = l1.next
-    } else if (l2 !== null) {
-      current.next = new ListNode(l2.val)
-      l2 = l2.next
-    } else {
-      break
-    }
-    current = current.next
-  }
-
-  if (l1 !== null) {
-    current.next = l1
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2)
+    return l1
   } else {
-    current.next = l2
+    l2.next = mergeTwoLists(l1, l2.next)
+    return l2
   }
-
-  return result.next
 }
 
 /** **** End Program ******/
