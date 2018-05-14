f14236c3a6543aa058297ceddf81d6f7b22e9635
levichen <levichen@nine-yi.com>
2018-05-14 09:14:05 +0800

    init 0021

---
+++ 0021_MergeTwoSortedLists.js
@@ -0,0 +1,32 @@
+// Definition for singly-linked list.
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  return true    
+};
+
+/****** End Program ******/
+
+/****** Start Testing ******/
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Test Merge Two Sorted Lists', function() {
+  it('', function() {
+    var input = 'Z';
+    var result = mergeTwoLists(input);
+  
+    expect(result).to.be.true;
+  });
+});
+
+/****** End Testing ******/
