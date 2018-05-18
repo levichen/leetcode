6c41752873a27116254de8b95566923579518994
levichen <levichen@nine-yi.com>
2018-05-18 13:11:45 +0800

    add 38

---
+++ 0038_CountAndSay.js
@@ -0,0 +1,34 @@
+// 1.     1
+// 2.     11
+// 3.     21
+// 4.     1211
+// 5.     111221
+// 6.     312211
+// 7.     13112221
+// 8.     1113213211
+// 9.     31131211131221
+// 10.   13211311123113112211
+
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function (n) {
+  return true
+}
+/** **** End Program ******/
+
+/** **** Start Testing ******/
+
+var chai = require('chai')
+var expect = chai.expect
+
+describe('Test Cases', function () {
+  it('Test1', function () {
+    var result = countAndSay()
+
+    expect(result).to.be.true
+  })
+})
+
+/** **** End Testing ******/
